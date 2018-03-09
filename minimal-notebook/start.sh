aba6bd7a9dc2206ec99f82e2e2ceda6e192425a0
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-03-09 12:23:40 +1100

    Add optional support for supervisord and ensure zombie processes are reaped when not using supervisord.

--- minimal-notebook/start.sh
+++ minimal-notebook/start.sh
@@ -7,5 +7,19 @@ if [ $# -eq 0 ]; then
     exec bash
 else
     echo "Executing the command: $@"
-    exec "$@"
+
+    if [[ -z "${JUPYTER_ENABLE_SUPERVISORD}" ]]; then
+        trap 'kill -TERM $PID' TERM INT
+
+        "$@" &
+
+        PID=$!
+        wait $PID
+        trap - TERM INT
+        wait $PID
+        STATUS=$?
+        exit $STATUS
+    else
+        exec "$@"
+    fi
 fi
