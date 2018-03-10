2e0e27730b2037eb93d1faa0af2d83e0e7b04ced
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-03-10 15:18:41 +1100

    Make copying of volume more robust to failures.

--- minimal-notebook/setup-volume.sh
+++ minimal-notebook/setup-volume.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
+set -x
+
+set -eo pipefail
+
 SRC=$1
 DEST=$2
 
 if [ -f $DEST/.delete-volume ]; then
+    rm -f $DEST.copied-volume
     rm -rf $DEST
 fi
 
-if [ -e $DEST ]; then
+if [ -f $DEST.copied-volume ]; then
    exit
 fi
 
-rsync --archive --no-perms $SRC/ $DEST
+mkdir $DEST
+
+tar -C $SRC -cf - . | tar -C $DEST -xvf -
+
+touch $DEST.copied-volume
