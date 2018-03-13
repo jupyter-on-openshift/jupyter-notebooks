a95ccdaa1929eab68e80a7f5d6b0add762bd10c5
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-03-13 17:14:28 +1100

    Add builtin support for running kernelgateway.

--- minimal-notebook/start-notebook.sh
+++ minimal-notebook/start-notebook.sh
@@ -2,14 +2,18 @@
 
 set -eo pipefail
 
-NOTEBOOK_ARGS="$NOTEBOOK_ARGS --config=/opt/app-root/etc/jupyter_notebook_config.py"
+NOTEBOOK_ARGS="--config=/opt/app-root/etc/jupyter_notebook_config.py"
 
 if [[ ! -z "${JUPYTERHUB_API_TOKEN}" ]]; then
-  exec /opt/app-root/bin/start-singleuser.sh "$@"
+    exec /opt/app-root/bin/start-singleuser.sh "$@"
 else
-  if [[ ! -z "${JUPYTER_ENABLE_LAB}" ]]; then
-    . /opt/app-root/bin/start.sh jupyter lab $NOTEBOOK_ARGS "$@"
-  else
-    . /opt/app-root/bin/start.sh jupyter notebook $NOTEBOOK_ARGS "$@"
-  fi
+    if [[ ! -z "${JUPYTER_ENABLE_KERNELGATEWAY}" ]]; then
+        . /opt/app-root/bin/start.sh jupyter kernelgateway "$@"
+    else
+        if [[ ! -z "${JUPYTER_ENABLE_LAB}" ]]; then
+            . /opt/app-root/bin/start.sh jupyter lab $NOTEBOOK_ARGS "$@"
+        else
+            . /opt/app-root/bin/start.sh jupyter notebook $NOTEBOOK_ARGS "$@"
+        fi
+    fi
 fi
