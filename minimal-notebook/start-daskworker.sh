1d132712e476c90acce18f48e743048e837ebe61
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2018-05-13 22:13:30 -0400

    Set memory limit.

--- minimal-notebook/start-daskworker.sh
+++ minimal-notebook/start-daskworker.sh
@@ -2,6 +2,9 @@
 
 set -eo pipefail
 
+export $(cgroup-limits)
+
 DASK_SCHEDULER_ADDRESS=${DASK_SCHEDULER_ADDRESS:-127.0.0.1:8786}
 
-. /opt/app-root/bin/start.sh dask-worker $DASK_SCHEDULER_ADDRESS $DASK_WORKER_ARGS "$@"
+. /opt/app-root/bin/start.sh dask-worker $DASK_SCHEDULER_ADDRESS \
+    --memory-limit $MEMORY_LIMIT_IN_BYTES $DASK_WORKER_ARGS "$@"
