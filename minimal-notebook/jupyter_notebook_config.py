125c9350a87d347a1da5a9d056fc7a55f132ab4b
Graham Dumpleton <Graham.Dumpleton@gmail.com>
2019-05-15 21:44:32 +1000

    Allow hosting via WebDav as sub URL path to notebook, rather than separate host/port.

--- minimal-notebook/jupyter_notebook_config.py
+++ minimal-notebook/jupyter_notebook_config.py
@@ -1,9 +1,14 @@
 import os
 
+port = int(os.environ.get('JUPYTER_NOTEBOOK_PORT', '8080'))
+
 c.NotebookApp.ip = '0.0.0.0'
-c.NotebookApp.port = 8080
+c.NotebookApp.port = port
 c.NotebookApp.open_browser = False
 c.NotebookApp.quit_button = False
+
+if os.environ.get('JUPYTERHUB_SERVICE_PREFIX'):
+    c.NotebookApp.base_url = os.environ.get('JUPYTERHUB_SERVICE_PREFIX')
 
 password = os.environ.get('JUPYTER_NOTEBOOK_PASSWORD')
 if password:
